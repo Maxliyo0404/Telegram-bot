8f5e09680ab570bf0f933e1d1b389ffa08629ac2
Mahliyo <gasandigital13@gmail.com>
2026-05-08 22:32:44 +0500

    Form

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -5,6 +5,9 @@ function Cards() {
   return (
     <div className="cards">
         <form className="form">
+            <label className=" label"> To'liq ism
+                <input type="text" />
+            </label>
             
         </form>
     </div>
