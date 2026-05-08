ca8388c818b7ef5b4b2bf0b50500b71247bac832
Mahliyo <gasandigital13@gmail.com>
2026-05-08 22:42:17 +0500

    telegram bot

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@
           
                 
 import React from 'react'
+import Cards from './Components/Cards/Cards'
 
 function App() {
   return (
     <>
-      
+      <Cards/>
     </>
   )
 }
