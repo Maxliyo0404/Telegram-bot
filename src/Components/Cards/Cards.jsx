baedef5db6321f9fbaa371ee75cb49af6520cff9
Mahliyo <gasandigital13@gmail.com>
2026-05-08 23:10:59 +0500

    submit

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -6,7 +6,7 @@ function Cards() {
        event.defaultPrevented();
        const token = "8660203377:AAHVRsKxi5fV8wIOJrFyr66R1tL-jj_LNmw"
        const chat_id = 647264939
-       const url "htpps://api.telegram"
+       const url `htpps://api.telegram.org/bot ${token}`
      }
     
   return (
