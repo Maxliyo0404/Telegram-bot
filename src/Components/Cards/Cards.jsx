ac726c7415626fe421d2747af940cac4ad42e86b
Mahliyo <gasandigital13@gmail.com>
2026-05-08 23:06:48 +0500

    submit

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -5,7 +5,7 @@ function Cards() {
      const sendMessage = (event) =>{
        event.defaultPrevented();
        const token = "8660203377:AAHVRsKxi5fV8wIOJrFyr66R1tL-jj_LNmw"
-       const chat_id = 
+       const chat_id = 647264939
      }
     
   return (
