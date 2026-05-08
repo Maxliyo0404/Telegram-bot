aa37d0b18d024fe1a2bdbdd7515a745073a3c21f
Mahliyo <gasandigital13@gmail.com>
2026-05-08 22:34:59 +0500

    Label

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -5,10 +5,12 @@ function Cards() {
   return (
     <div className="cards">
         <form className="form">
-            <label className=" label"> To'liq ism
+            <label className=" label"> 
+                To'liq ism
                 <input className="input" type="text" required placeholder="Ismingizni kiriting"/>
             </label>
-            <label className=" label"> To'liq ism
+            <label className=" label">
+                Telefon raqam
                 <input className="input" type="text" required placeholder="Ismingizni kiriting"/>
             </label>
             
